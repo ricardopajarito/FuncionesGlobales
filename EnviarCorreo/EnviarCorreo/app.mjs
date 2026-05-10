768a0b2787fa27b06852ca4db3eb5c59710b28e1
“ricardopajarito” <ricardopch15@gmail.com>
2026-05-10 13:47:06 -0600

    validacion de recaptcha

--- EnviarCorreo/EnviarCorreo/app.mjs
+++ EnviarCorreo/EnviarCorreo/app.mjs
@@ -1,24 +1,60 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+import { config } from "./config.mjs";
+let statusCode = 400;
+let mensaje = "";
 
 export const handler = async (event, context) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      })
-    };
 
-    return response;
+
+  try {
+
+    const body = typeof event.body === "string" ? JSON.parse(event.body) : event.body;
+    const { datosFormulario, plantilla } = body;
+
+    if (!datosFormulario || !plantilla) {
+      throw new Error("No se proporcionaron datos del formulario o la plantilla.");
+    }
+
+    const recaptchaResponse = datosFormulario["g-recaptcha-response"];
+
+    if (!recaptchaResponse) {
+      throw new Error("No se proporcionó la respuesta del recaptcha.");
+    }
+
+    const secretKey = config.RECAPTCHA_SECRET_KEY; // Clave secreta oculta
+
+    // Verificar con Google
+    const response = await fetch(config.URL_CAPTCHA_GOOGLE, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `secret=${secretKey}&response=${recaptchaResponse}`,
+    });
+
+    const result = await response.json();
+
+    if (!result.success) {
+      throw new Error("Error de validación");
+    }
+
+    // Enviar correo
+
+  } catch (error) {
+    console.error("Error en el handler:", error);
+    statusCode = 500;
+    mensaje = "Error al enviar el correo: " + error.message;
+  }
+
+  const response = {
+    statusCode,
+    body: JSON.stringify({
+      mensaje
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
   };
-  
+
+  return response;
+};
---
+++ EnviarCorreo/EnviarCorreo/config.mjs
@@ -0,0 +1,4 @@
+export const config = {
+    URL_CAPTCHA_GOOGLE: 'https://www.google.com/recaptcha/api/siteverify',
+    RECAPTCHA_SECRET_KEY: ""
+};
