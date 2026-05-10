a143cee7bb5a7f2fad4612fb29867b35b8eadc04
“ricardopajarito” <ricardopch15@gmail.com>
2026-05-10 14:38:00 -0600

    logica para enviar correos

--- EnviarCorreo/EnviarCorreo/app.mjs
+++ EnviarCorreo/EnviarCorreo/app.mjs
@@ -1,12 +1,18 @@
-import { config } from "./config.mjs";
+import { Resend } from 'resend';
+import { config, datosCorreo } from "./config.mjs";
+
+const resend = new Resend(config.RESEND_API_KEY);
 let statusCode = 400;
 let mensaje = "";
 
 export const handler = async (event, context) => {
-
+  console.log("Evento recibido:", event);
 
   try {
 
+    if (event.httpMethod !== "POST") {
+      throw new Error("Método no permitido");
+    }
     const body = typeof event.body === "string" ? JSON.parse(event.body) : event.body;
     const { datosFormulario, plantilla } = body;
 
@@ -38,11 +44,14 @@ export const handler = async (event, context) => {
     }
 
     // Enviar correo
+    await enviarCorreo(datosFormulario, plantilla);
 
+    statusCode = 200;
+    mensaje = "Correo enviado exitosamente";
   } catch (error) {
     console.error("Error en el handler:", error);
     statusCode = 500;
-    mensaje = "Error al enviar el correo: " + error.message;
+    mensaje = "Error al enviar el correo. Por favor, inténtalo de nuevo más tarde.";
   }
 
   const response = {
@@ -58,3 +67,39 @@ export const handler = async (event, context) => {
 
   return response;
 };
+
+async function enviarCorreo(datosFormulario, plantilla) {
+
+  let dominio = "http://agenciatwowolves.com.mx/";
+  if (datosCorreo[dominio]) {
+    let datosRemitente = datosCorreo[dominio].datosRemitente;
+    let datosDestinatario = datosCorreo[dominio].datosDestinatario;
+    let plantillaHTML = datosCorreo[dominio].plantillas[plantilla];
+
+    if (!plantillaHTML) {
+      throw new Error("Plantilla no encontrada.");
+    }
+
+    if (!datosRemitente || !datosDestinatario) {
+      throw new Error("Datos de remitente o destinatario no encontrados.");
+    }
+
+    let payload = {
+      from: `${datosRemitente.nombre} <${datosRemitente.correo}>`,
+      to: [`${datosDestinatario.correo}`],
+      subject: plantillaHTML.asunto,
+      html: plantillaHTML.html(datosFormulario)
+    };
+    console.log("Payload para Resend:", payload);
+
+    const { data, error } = await resend.emails.send(payload);
+  
+    if (error) {
+      console.error(error);
+    }
+  
+    console.log({ data });
+  } else {
+    throw new Error("Accion no permitida.");
+  }
+}
--- EnviarCorreo/EnviarCorreo/config.mjs
+++ EnviarCorreo/EnviarCorreo/config.mjs
@@ -1,4 +1,49 @@
 export const config = {
     URL_CAPTCHA_GOOGLE: 'https://www.google.com/recaptcha/api/siteverify',
-    RECAPTCHA_SECRET_KEY: ""
+    RECAPTCHA_SECRET_KEY: "",
+    RESEND_API_KEY: ""
 };
+
+export const datosCorreo = {
+    "https://mihotelsanjosepuebla.com/": {
+        "datosRemitente": {
+            "nombre": "Hotel San José Puebla",
+            "correo": "no-reply@mihotelsanjosepuebla.com"
+        },
+        "datosDestinatario": {
+            "correo": "ricardopch15@gmail.com"
+        },
+        "plantillas": {
+            "p-contacto": {
+                "asunto": "Nuevo mensaje de contacto",
+                "html": "plantilla-contacto.html"
+            }
+        }
+    },
+    "http://agenciatwowolves.com.mx/": {
+        "datosRemitente": {
+            "nombre": "Agencia Two Wolves",
+            "correo": "no-reply@agenciatwowolves.com.mx"
+        },
+        "datosDestinatario": {
+            "correo": "ricardopch15@gmail.com"
+        },
+        "plantillas": {
+            "p-contacto": {
+                "asunto": "Nuevo mensaje de contacto",
+                "html": (payload) => {
+                    return `
+                        <h2>Nuevo mensaje de Contacto</h2>
+                        <p><strong>Nombre:</strong> ${payload.name} ${payload.lastname}</p>
+                        <p><strong>Email:</strong> ${payload.email}</p>
+                        <p><strong>Teléfono:</strong> ${payload?.phone || 'No proporcionado'}</p>
+                        <p><strong>Compañía:</strong> ${payload?.company || 'No proporcionado'}</p>
+                        <p><strong>¿En qué servicio está interesado?:</strong> ${payload?.services || 'No proporcionado'}</p>
+                        <p><strong>Comentarios de su proyecto:</strong> ${payload?.message || 'No proporcionado'}</p>
+                        <p><strong>¿Cómo nos conoció?:</strong><br/>${payload?.media || 'No proporcionado'}</p>
+                    `
+                }
+            }
+        }
+    }
+}
